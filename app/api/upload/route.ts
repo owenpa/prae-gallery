94a33b6fe7b47450e54934a1b5ea56a11fc91ea4
Owen Hill <on-hill@outlook.com>
2024-02-13 12:16:21 -0600

    file upload/save

---
+++ app/api/upload/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { writeFile } from 'fs/promises'
+import path from 'path'
+import { addImageInfoToDB } from '../../../actions'
+import type { ImageDataToDB } from '@/types/types'
+
+export async function POST (req: NextRequest):
+Promise<NextResponse<{ error: string }> | NextResponse<{ Message: string, status: number }>> {
+  const formData = await req.formData()
+  const formMetadata: ImageDataToDB = Object.fromEntries((new URL(req.url)).searchParams)
+  const file = formData.get('file')
+  if (file === null) { return NextResponse.json({ error: 'No files received.' }, { status: 400 }) }
+  const buffer = Buffer.from(await (file as File).arrayBuffer())
+  const filename = (file as File).name.replaceAll(' ', '_')
+  try {
+    await writeFile(path.join(process.cwd(), 'public/gallery/' + filename), buffer)
+    await addImageInfoToDB(formMetadata)
+    return NextResponse.json({ Message: 'Success', status: 201 })
+  } catch (error) {
+    console.error('Error occurred during image saving: ', error)
+    return NextResponse.json({ Message: 'Failed', status: 500 })
+  }
+}

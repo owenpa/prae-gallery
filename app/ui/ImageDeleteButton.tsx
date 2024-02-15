e01d241c3dfddf0d5e8b42dcecf50b1a2493afef
Owen Hill <on-hill@outlook.com>
2024-02-15 12:51:14 -0600

    re adding?

---
+++ app/ui/ImageDeleteButton.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+export default function ImageDeleteButton ({ parentFileName }: { parentFileName: string }): JSX.Element {
+  function openModal (): void {
+    const confirmationModal = document.getElementById('confirmation-modal');
+    (confirmationModal as HTMLDialogElement).showModal()
+    const displayImageName = document.getElementById('display-image-name')
+    if (displayImageName !== null) {
+      displayImageName.innerText = parentFileName
+    }
+  }
+
+  return (
+    <>
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" id={`button-${parentFileName}`} onClick={() => { openModal() }}>
+        Delete
+      </button>
+    </>
+  )
+}

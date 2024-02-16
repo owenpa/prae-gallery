dac119be91c122275f27236ef3cf8394dfa8ae28
Owen Hill <on-hill@outlook.com>
2024-02-16 14:45:29 -0600

    move content into client component for interactivity

--- app/imageviewer/ImageView.tsx
+++ app/imageviewer/ImageView.tsx
@@ -1,25 +1,12 @@
 'use server'
-import Image from 'next/image'
+
 import { pullImageInfoFromDb } from '../../actions'
+import ImagePostContainer from './ImagePostContainer'
 
 export default async function ImageView (): Promise<JSX.Element> {
   const imageInfoArray = await pullImageInfoFromDb()
-  let imagePost: JSX.Element[] = [<></>]
-  if (imageInfoArray.length !== 0) {
-    imagePost = imageInfoArray.map((fileObj, idx) => {
-      return (
-        <div key={idx}>
-          <h1>{fileObj.imagename}</h1>
-          <Image width={'100'} height={'100'} src={`/assets/gallery/${fileObj.imagename}`} alt={`image in gallery #${idx}`} />
-          <p>{fileObj.description}</p>
-        </div>
-      )
-    })
-  }
 
   return (
-    <div>
-            {imageInfoArray.length > 0 ? imagePost : <p>No images to display.</p>}
-    </div>
+    <ImagePostContainer imageInfoArray={imageInfoArray} />
   )
 }

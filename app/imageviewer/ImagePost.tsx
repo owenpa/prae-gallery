cf2ea4e873a8005432228b36397e7f6c76d6c36f
Owen Hill <on-hill@outlook.com>
2024-02-17 15:44:49 -0600

    move buttons into separate component

--- app/imageviewer/ImagePost.tsx
+++ app/imageviewer/ImagePost.tsx
@@ -1,16 +1,15 @@
 'use client'
 
 import Image from 'next/image'
-import { Button } from '../ui/button'
+import ActionBar from '../ui/ActionBar'
+import { useEffect, useState } from 'react'
 
 export default function ImagePost ({ setIndexProp, idx, imagesrc, imagename, imagedesc }: { setIndexProp: (prevIdx: number, newIdx: number, direction: string) => void, idx: number, imagesrc: string, imagename: string, imagedesc: string }): JSX.Element {
-  function handleUpClick (): void {
-    setIndexProp(idx, idx + 1, 'up')
-  }
-
-  function handleDownClick (): void {
-    setIndexProp(idx, idx - 1, 'down')
-  }
+  const [previouslyLiked, setPreviouslyLiked] = useState(false)
+  useEffect(() => {
+    setPreviouslyLiked(localStorage.getItem(imagename) !== null)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className='grid grid-cols-[50vw_40vw] absolute center w-full justify-center h-screen'>
@@ -20,8 +19,7 @@ export default function ImagePost ({ setIndexProp, idx, imagesrc, imagename, ima
       <div className='max-w self-center'>
         <p className='text-3xl break-all'>{imagename}</p>
         <p>{imagedesc}</p>
-        <Button className='nav-button' onClick={() => { handleUpClick() }}>Up</Button>
-        <Button className='nav-button' onClick={() => { handleDownClick() }}>Down</Button>
+        <ActionBar setIndexProp={setIndexProp} idx={idx} imagename={imagename} previouslyLiked={previouslyLiked}/>
       </div>
     </div>
   )
---
+++ app/ui/ActionBar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { Heart, Link } from 'lucide-react'
+import { Button } from '../ui/button'
+import type { MouseEvent } from 'react'
+
+export default function ActionBar ({ setIndexProp, idx, imagename, previouslyLiked }: { setIndexProp: (prevIdx: number, newIdx: number, direction: string) => void, idx: number, imagename: string, previouslyLiked: boolean }): JSX.Element {
+  function handleUpClick (): void {
+    setIndexProp(idx, idx + 1, 'up')
+  }
+
+  function handleDownClick (): void {
+    setIndexProp(idx, idx - 1, 'down')
+  }
+
+  function handleLikeClick (clickEvent: MouseEvent<HTMLDivElement>): void {
+    (clickEvent.target as HTMLElement).firstElementChild?.classList.toggle('fill-red-400')
+    if (localStorage.getItem(imagename) !== null) {
+      localStorage.removeItem(imagename)
+    } else {
+      localStorage.setItem(imagename, '1')
+      try {
+        void fetch('/api/likepost', {
+          method: 'POST',
+          body: JSON.stringify(imagename)
+        })
+        // execute like animation on successful response
+      } catch (error) {
+        console.error(`Error liking image ${imagename}`)
+      }
+    }
+  }
+
+  return (
+    <div className='flex w-full content-evenly flex-wrap justify-evenly'>
+      <Button className='nav-button p-' onClick={() => { handleUpClick() }}>Up</Button>
+      <div onClick={(clickEvent: MouseEvent<HTMLDivElement>) => { handleLikeClick(clickEvent) }} className='flex'>
+        <button>
+          <Heart color='#ff7a7a' className={`hover:fill-red-400 pointer-events-none ${previouslyLiked ? 'fill-red-400' : ''}`} />
+        </button>
+      </div>
+      <button><Link className='hover:stroke-blue-300' /></button>
+      <Button className='nav-button' onClick={() => { handleDownClick() }}>Down</Button>
+    </div>
+  )
+}

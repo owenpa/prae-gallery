23966587f7fb04f0e1f1c2d6dbbeac7971d13710
Owen Hill <on-hill@outlook.com>
2024-02-19 14:53:22 -0600

    text contact over form contact

--- app/commission/CommissionView.tsx
+++ app/commission/CommissionView.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react'
 import Image from 'next/image'
+import { X } from 'lucide-react'
 
 export default function CommissionView ({ imageNameList }: { imageNameList: string[] }): JSX.Element {
   const imageList = imageNameList.map((imageName, idx) => {
@@ -14,7 +15,7 @@ export default function CommissionView ({ imageNameList }: { imageNameList: stri
 
   let index = 0
   function changeDisplayedImage (): void {
-    const images = document.getElementById('image-post-container')?.children
+    const images = document.getElementById('gallery-image-container')?.children
     if (images === undefined) {
       return
     }
@@ -30,10 +31,22 @@ export default function CommissionView ({ imageNameList }: { imageNameList: stri
   useEffect(() => changeDisplayedImage, [])
   return (
     <div className='flex w-full h-screen'>
-      <div id='image-post-container' className='flex w-3/4 items-center justify-center relative'>
+      <div id='gallery-image-container' className='flex w-3/4 items-center justify-center relative'>
       {imageList.length > 1 ? imageList : <p>No images to display.</p>}
       </div>
       <div className='flex h-full w-1/4 items-center'>
+        <div>
+          <h1 className='text-xl'>Commission status: <X className='inline' color='#ff7a7a' /></h1>
+          Prices may vary:
+          <ul>
+            <li>$$ : ???</li>
+            <li>$$ : ???</li>
+            <li>$$ : ???</li>
+            <li>$$ : ???</li>
+          </ul>
+          <p>Contact me here: </p>
+          <a href='mailto:someone@example.com'>someone@example.com</a>
+        </div>
       </div>
     </div>
   )

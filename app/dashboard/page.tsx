6895cb961ddcf668db6a023aac59bd4dc97649d7
Owen Hill <on-hill@outlook.com>
2024-02-19 15:04:05 -0600

    add logout button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,17 @@
 import ImageUpload from './ImageUpload'
 import DBImageControl from './DBImageControl'
+import { Button } from '../ui/button'
+import { signOut } from '../../auth'
 export default function ControlPanel (): JSX.Element {
   return (
     <div>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <form action={async (): Promise<void> => {
+          'use server'
+          await signOut()
+        }}>
+          <Button> Sign Out</Button>
+        </form>
       <ImageUpload/>
       <DBImageControl/>
     </div>

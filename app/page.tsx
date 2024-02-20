d132b7b02e7ff54c0320f4c14667b524bcaa4d29
Owen Hill <on-hill@outlook.com>
2024-02-19 18:56:28 -0600

    move links

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import Link from 'next/link'
-import ToggleTheme from './ui/ToggleTheme'
 import ImageView from './imageviewer/ImageView'
 
 export default async function Home (): Promise<JSX.Element> {
   return (
     <main>
-      <p className='absolute top-0 right-0 z-50'>
-        <ToggleTheme />
-        <Link href='/commission'>commission</Link> -
-        <Link href='login'>login</Link>
-      </p>
       <ImageView />
     </main>
   )

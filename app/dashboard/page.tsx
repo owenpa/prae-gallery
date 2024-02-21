431827d26b7e1c4c84863ebd7fa5e928c4fcc9f3
Owen Hill <on-hill@outlook.com>
2024-02-21 15:36:40 -0600

    vertical align

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,9 +4,9 @@ import { Button } from '../ui/button'
 import { signOut } from '../../auth'
 export default function ControlPanel (): JSX.Element {
   return (
-    <div>
+    <div className='flex flex-col items-center'>
         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-        <form className='text-right' action={async (): Promise<void> => {
+        <form className='text-right self-end' action={async (): Promise<void> => {
           'use server'
           await signOut()
         }}>

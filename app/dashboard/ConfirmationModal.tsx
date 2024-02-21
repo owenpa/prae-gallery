ddfe8ec23e4f18542c153a2088189b7b07b91ce1
Owen Hill <on-hill@outlook.com>
2024-02-21 15:22:08 -0600

    remove undefined logs

--- app/dashboard/ConfirmationModal.tsx
+++ app/dashboard/ConfirmationModal.tsx
@@ -7,7 +7,9 @@ import { deleteImageInfoFromDb } from '../../actions'
 export default function ConfirmationModal (): JSX.Element {
   const submitButtonColors = { red: 'bg-red-500 hover:bg-red-700 text-white p-3', green: 'bg-green-500 text-white hover:bg-green-700 p-3' }
   const [errorMessage, fireDeleteImage] = useFormState(deleteImageInfoFromDb, undefined)
-  console.log(errorMessage)
+  if (errorMessage !== undefined) {
+    console.log(errorMessage)
+  }
   function imageNameOnChange (onChangeEvent: ChangeEvent<HTMLInputElement>): number {
     const submitButton = document.getElementById('submit-delete')
     const imageName = document.getElementById('display-image-name')?.innerText

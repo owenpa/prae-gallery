ab39b146440832d04f1034693fd73d53946df7ba
Owen Hill <on-hill@outlook.com>
2024-02-24 16:41:22 -0600

    retrieve and display statistics

--- app/ui/AnalyticsBar.tsx
+++ app/ui/AnalyticsBar.tsx
@@ -6,7 +6,41 @@ import { useState } from 'react'
 export default function AnalyticsBar ({ data }: { data: any[] }): JSX.Element {
   const [showAnalytics, setShowAnalytics] = useState(false)
 
-  return (
+  function findTotalPosts (): number {
+    return data.length
+  }
+
+  function findTotalLikes (): number {
+    return data.reduce((likeTotal: number, currData) => likeTotal + (currData.likes as number), 0)
+  }
+
+  function findTotalShares (): number {
+    return data.reduce((shareTotal: number, currData) => shareTotal + (currData.shares as number), 0)
+  }
+  function findAvgLikes (): number {
+    if (totalLikes === 0) { return 0 }
+    return (Math.round((totalPosts / totalLikes) + Number.EPSILON) * 100) / 100
+  }
+
+  function findAvgShares (): number {
+    if (totalShares === 0) { return 0 }
+    return (Math.round((totalPosts / totalShares) + Number.EPSILON) * 100) / 100
+  }
+
+  const totalPosts = findTotalPosts()
+  const totalLikes = findTotalLikes()
+  const totalShares = findTotalShares()
+  const avgLikes = findAvgLikes()
+  const avgShares = findAvgShares()
+
+  if (data.length === 0) {
+    return (
+      <div className='border m-2 flex flex-col px-4 py-6 rounded-lg tracking-wide max-h-[200px] max-w-xl self-center w-full'>
+        <p>No images or analytics found in the database.</p>
+      </div>
+    )
+  } else {
+    return (
     <>
     <Button onClick={() => {
       setShowAnalytics(!showAnalytics)
@@ -22,19 +56,19 @@ export default function AnalyticsBar ({ data }: { data: any[] }): JSX.Element {
             <li>
               <div>
                 <p># of posts</p>
-                <p id='total-post'>0</p>
+                <p id='total-post'>{totalPosts}</p>
               </div>
             </li>
             <li>
               <div>
                 <p># of likes</p>
-                <p id='total-like'>1</p>
+                <p id='total-like'>{totalLikes}</p>
               </div>
             </li>
             <li>
               <div>
                 <p># of shares</p>
-                <p id='total-share'>2</p>
+                <p id='total-share'>{totalShares}</p>
               </div>
             </li>
           </ul>
@@ -42,18 +76,19 @@ export default function AnalyticsBar ({ data }: { data: any[] }): JSX.Element {
             <li>
               <div>
                 <p>avg. likes per post</p>
-                <p id='avg-like-per'>3</p>
+                <p id='avg-like-per'>{avgLikes}</p>
               </div>
             </li>
             <li>
               <div>
                 <p>avg. shares per post</p>
-                <p id='avg-shares-per'>4</p>
+                <p id='avg-shares-per'>{avgShares}</p>
               </div>
             </li>
           </ul>
         </div>
       }
       </>
-  )
+    )
+  }
 }

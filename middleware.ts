2d9916dd216a9a91d8a5c191b09f110b4b1eee5b
Owen Hill <on-hill@outlook.com>
2024-02-24 14:56:42 -0600

    now checks for authentication for all api requests except for /likepost

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { authConfig } from './auth.config'
 export default NextAuth(authConfig).auth
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+  matcher: ['/((?!api/likepost|_next/static|_next/image|.*\\.png$).*)']
 }

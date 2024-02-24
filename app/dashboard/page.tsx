5177af69fd78388c4b148d15090cbbd1617a5b8a
Owen Hill <on-hill@outlook.com>
2024-02-24 17:08:31 -0600

    move signout button to dash navbar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,9 @@
 import ImageUpload from './ImageUpload'
 import DBImageControl from './DBImageControl'
 import PreviewAnalytics from './PreviewAnalytics'
-import { Button } from '../ui/button'
-import { signOut } from '../../auth'
 export default function ControlPanel (): JSX.Element {
   return (
     <div className='flex flex-col items-center'>
-        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-        <form className='text-right self-end' action={async (): Promise<void> => {
-          'use server'
-          await signOut()
-        }}>
-          <Button>Sign Out</Button>
-        </form>
       <ImageUpload/>
       <DBImageControl/>
       <PreviewAnalytics />
--- app/ui/DashboardNavbar.tsx
+++ app/ui/DashboardNavbar.tsx
@@ -1,7 +1,19 @@
+import { signOut } from '@/auth'
+import { Button } from './button'
+
 export default function DashboardNavbar (): JSX.Element {
   return (
-    <ul>
-      <p>link</p>
-    </ul>
+    <>
+      <ul className='flex justify-between'>
+        <p>link</p>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <form className='text-right self-end' action={async (): Promise<void> => {
+          'use server'
+          await signOut()
+        }}>
+          <Button>Sign Out</Button>
+        </form>
+      </ul>
+    </>
   )
 }

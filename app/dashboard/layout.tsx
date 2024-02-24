6740663a44a24f316a39e22a132a1278ee29e56b
Owen Hill <on-hill@outlook.com>
2024-02-24 17:01:15 -0600

    remove sidenav in dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,6 @@
-import SideDashboardNav from '../ui/SideDashboardNav'
-
 export default function Layout ({ children }: Readonly<{ children: React.ReactNode }>): JSX.Element {
   return (
     <div className='flex'>
-      <SideDashboardNav />
       <main className='w-full'>{children}</main>
     </div>
   )

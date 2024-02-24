0263e5b6d79e43277d98f1720a93e464e05741a2
Owen Hill <on-hill@outlook.com>
2024-02-24 17:33:58 -0600

    small styling / prefill links

--- app/ui/DashboardNavbar.tsx
+++ app/ui/DashboardNavbar.tsx
@@ -1,19 +1,26 @@
 import { signOut } from '@/auth'
 import { Button } from './button'
+import Link from 'next/link'
+import { MoveLeft } from 'lucide-react'
 
 export default function DashboardNavbar (): JSX.Element {
   return (
-    <>
-      <ul className='flex justify-between'>
-        <p>link</p>
-        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-        <form className='text-right self-end' action={async (): Promise<void> => {
-          'use server'
-          await signOut()
-        }}>
+    <nav className='flex flex-wrap justify-evenly p-6'>
+      <ul className='flex self-center justify-center gap-10'>
+      <Link href={'/dashboard'} className='flex gap-2' ><MoveLeft /> Back To Gallery</Link>
+        <Link href={'/dashboard'}>General Overview</Link>
+        <Link href={'/upload'}>Upload</Link>
+        <Link href={'/manage'}>Manage Posts</Link>
+        <Link href={'/analytics'}>Analytics</Link>
+        <Link href={'/view'}>View All</Link>
+      </ul>
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+      <form className='text-right self-end' action={async (): Promise<void> => {
+        'use server'
+        await signOut()
+      }}>
           <Button>Sign Out</Button>
         </form>
-      </ul>
-    </>
+    </nav>
   )
 }

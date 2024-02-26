bdc11bfe64e2e70ff971944f7d8c279668947780
Owen Hill <on-hill@outlook.com>
2024-02-26 10:21:38 -0600

    toggle scrolling instead of disabling the action bar when one image is present

--- app/ui/ActionBar.tsx
+++ app/ui/ActionBar.tsx
@@ -4,7 +4,7 @@ import { Heart, Link } from 'lucide-react'
 import { Button } from '../ui/button'
 import type { MouseEvent } from 'react'
 
-export default function ActionBar ({ setIndexProp, idx, imagename, previouslyLiked }: { setIndexProp: ((prevIdx: number, newIdx: number, direction: string) => void) | undefined, idx: number, imagename: string, previouslyLiked: boolean }): JSX.Element {
+export default function ActionBar ({ oneGalleryImage, setIndexProp, idx, imagename, previouslyLiked }: { oneGalleryImage: boolean, setIndexProp: ((prevIdx: number, newIdx: number, direction: string) => void) | undefined, idx: number, imagename: string, previouslyLiked: boolean }): JSX.Element {
   function handleUpClick (): void {
     setIndexProp !== undefined && setIndexProp(idx, idx + 1, 'up')
   }
@@ -32,15 +32,15 @@ export default function ActionBar ({ setIndexProp, idx, imagename, previouslyLik
   }
 
   return (
-    <div className='flex w-full flex-wrap justify-between max-w-80'>
-      <Button className='nav-button p-' onClick={() => { handleUpClick() }}>Up</Button>
+    <div className='flex w-full flex-wrap justify-between max-w-80 h-10'>
+      {!oneGalleryImage && <Button className='nav-button' onClick={() => { handleUpClick() }}>Up</Button>}
       <div onClick={(clickEvent: MouseEvent<HTMLDivElement>) => { handleLikeClick(clickEvent) }} className='flex'>
         <button>
           <Heart color='#ff7a7a' className={`hover:fill-red-400 pointer-events-none ${previouslyLiked ? 'fill-red-400' : ''}`} />
         </button>
       </div>
       {/* <button><Link className='hover:stroke-blue-300' /></button> */}
-      <Button className='nav-button' onClick={() => { handleDownClick() }}>Down</Button>
+     {!oneGalleryImage && <Button className='nav-button' onClick={() => { handleDownClick() }}>Down</Button>}
     </div>
   )
 }

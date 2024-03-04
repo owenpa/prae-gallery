06239ce5ab5c24cd9ea9c48f5dea0596d1b3d516
Owen Hill <on-hill@outlook.com>
2024-03-04 11:42:26 -0600

    display link button, add toggle for centering items in actionbar

--- app/ui/ActionBar.tsx
+++ app/ui/ActionBar.tsx
@@ -4,7 +4,7 @@ import { Heart, Link } from 'lucide-react'
 import { Button } from '../ui/button'
 import type { MouseEvent } from 'react'
 
-export default function ActionBar ({ oneGalleryImage, setIndexProp, idx, imagename, previouslyLiked }: { oneGalleryImage: boolean, setIndexProp: ((prevIdx: number, newIdx: number, direction: string) => void) | undefined, idx: number, imagename: string, previouslyLiked: boolean }): JSX.Element {
+export default function ActionBar ({ centerItems, oneGalleryImage, setIndexProp, idx, imagename, previouslyLiked }: { centerItems: boolean, oneGalleryImage: boolean, setIndexProp: ((prevIdx: number, newIdx: number, direction: string) => void) | undefined, idx: number, imagename: string, previouslyLiked: boolean }): JSX.Element {
   function handleUpClick (): void {
     setIndexProp !== undefined && setIndexProp(idx, idx + 1, 'up')
   }
@@ -32,14 +32,14 @@ export default function ActionBar ({ oneGalleryImage, setIndexProp, idx, imagena
   }
 
   return (
-    <div className='flex w-full flex-wrap justify-between max-w-80 h-10'>
+    <div className={`flex w-full flex-wrap h-10 ${oneGalleryImage ? 'gap-2' : 'justify-between max-w-80'} ${centerItems ? 'justify-center' : ''}`}>
       {!oneGalleryImage && <Button className='nav-button' onClick={() => { handleUpClick() }}>Up</Button>}
       <div onClick={(clickEvent: MouseEvent<HTMLDivElement>) => { handleLikeClick(clickEvent) }} className='flex'>
         <button>
           <Heart color='#ff7a7a' className={`hover:fill-red-400 pointer-events-none ${previouslyLiked ? 'fill-red-400' : ''}`} />
         </button>
       </div>
-      {/* <button><Link className='hover:stroke-blue-300' /></button> */}
+      <button><Link className='hover:stroke-blue-300' /></button>
      {!oneGalleryImage && <Button className='nav-button' onClick={() => { handleDownClick() }}>Down</Button>}
     </div>
   )

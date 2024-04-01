3c4ff99b413472692460797fbc02ee90983818be
Owen Hill <on-hill@outlook.com>
2024-04-01 16:47:53 -0500

    add ability to toggle toggling

--- app/ui/AnalyticsBar.tsx
+++ app/ui/AnalyticsBar.tsx
@@ -3,7 +3,7 @@ import { ChevronUp } from 'lucide-react'
 import { Button } from '../ui/button'
 import { useState } from 'react'
 
-export default function AnalyticsBar ({ data }: { data: any[] }): JSX.Element {
+export default function AnalyticsBar ({ data, toggleable }: { data: any[], toggleable: boolean }): JSX.Element {
   const [showAnalytics, setShowAnalytics] = useState(false)
 
   function findTotalPosts (): number {
@@ -40,55 +40,98 @@ export default function AnalyticsBar ({ data }: { data: any[] }): JSX.Element {
       </div>
     )
   } else {
-    return (
-    <>
-    <Button onClick={() => {
-      setShowAnalytics(!showAnalytics)
-    }} variant={'ghost'} className='py-10 pt-7 max-w-96'>
-        <div className='flex flex-col text-center items-center justify-center'>
-          <ChevronUp size={32} className='' />
-          <p>Preview Analytics</p>
-        </div>
-      </Button>
-      {showAnalytics &&
-        <div className='border m-2 flex flex-col px-4 py-6 rounded-lg tracking-wide max-h-[200px] max-w-xl self-center w-full'>
-          <ul className='flex justify-evenly'>
-            <li>
-              <div>
-                <p># of posts</p>
-                <p id='total-post'>{totalPosts}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <p># of likes</p>
-                <p id='total-like'>{totalLikes}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <p># of shares</p>
-                <p id='total-share'>{totalShares}</p>
-              </div>
-            </li>
-          </ul>
-          <ul className='flex justify-evenly '>
-            <li>
-              <div>
-                <p>avg. likes per post</p>
-                <p id='avg-like-per'>{avgLikes}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <p>avg. shares per post</p>
-                <p id='avg-shares-per'>{avgShares}</p>
-              </div>
-            </li>
-          </ul>
-        </div>
-      }
-      </>
-    )
+    if (toggleable) {
+      return (
+        <>
+        <Button onClick={() => {
+          setShowAnalytics(!showAnalytics)
+        }} variant={'ghost'} className='py-10 pt-7 max-w-96'>
+            <div className='flex flex-col text-center items-center justify-center'>
+              <ChevronUp size={32} className='' />
+              <p>Preview Analytics</p>
+            </div>
+          </Button>
+          {showAnalytics &&
+            <div className='border m-2 flex flex-col px-4 py-6 rounded-lg tracking-wide max-h-[200px] max-w-xl self-center w-full'>
+              <ul className='flex justify-evenly'>
+                <li>
+                  <div>
+                    <p># of posts</p>
+                    <p id='total-post'>{totalPosts}</p>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <p># of likes</p>
+                    <p id='total-like'>{totalLikes}</p>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <p># of shares</p>
+                    <p id='total-share'>{totalShares}</p>
+                  </div>
+                </li>
+              </ul>
+              <ul className='flex justify-evenly '>
+                <li>
+                  <div>
+                    <p>avg. likes per post</p>
+                    <p id='avg-like-per'>{avgLikes}</p>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <p>avg. shares per post</p>
+                    <p id='avg-shares-per'>{avgShares}</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          }
+          </>
+      )
+    } else {
+      return (
+        <>
+          <div className='border m-2 flex flex-col px-4 py-6 rounded-lg tracking-wide max-h-[200px] max-w-xl self-center w-full'>
+            <ul className='flex justify-evenly'>
+              <li>
+                <div>
+                  <p># of posts</p>
+                  <p id='total-post'>{totalPosts}</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <p># of likes</p>
+                  <p id='total-like'>{totalLikes}</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <p># of shares</p>
+                  <p id='total-share'>{totalShares}</p>
+                </div>
+              </li>
+            </ul>
+            <ul className='flex justify-evenly '>
+              <li>
+                <div>
+                  <p>avg. likes per post</p>
+                  <p id='avg-like-per'>{avgLikes}</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <p>avg. shares per post</p>
+                  <p id='avg-shares-per'>{avgShares}</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </>
+      )
+    }
   }
 }

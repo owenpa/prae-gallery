89838e7343bf1e304777128ddb66241087ac262e
Owen Hill <on-hill@outlook.com>
2024-04-01 12:26:19 -0500

    begin creating createpost page

--- auth.config.ts
+++ auth.config.ts
@@ -10,12 +10,18 @@ export const authConfig = {
       const isLoggedIn = !!auth?.user
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
       const isOnLoginPage = nextUrl.pathname.startsWith('/login')
+      const isUploading = nextUrl.pathname.startsWith('/upload')
+      const isOnCreatePostPage = nextUrl.pathname.startsWith('/create')
       const isFetchingAnalytics = nextUrl.pathname.startsWith('/api/analytics')
       if (isOnDashboard) {
         return isLoggedIn
       } else if (isOnLoginPage && isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl))
       } else if (isFetchingAnalytics && !isLoggedIn) {
+        return Response.redirect(new URL('/', nextUrl))
+      } else if (isUploading && !isLoggedIn) {
+        return Response.redirect(new URL('/', nextUrl))
+      } else if (isOnCreatePostPage && !isLoggedIn) {
         return Response.redirect(new URL('/', nextUrl))
       }
       return true

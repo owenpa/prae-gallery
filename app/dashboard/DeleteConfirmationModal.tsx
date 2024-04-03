57fe5c591af409ec2b0d8cf80231e924c586aed2
Owen Hill <on-hill@outlook.com>
2024-04-03 11:12:09 -0500

    change file name to deleteconfirmationmodal

--- app/dashboard/DeleteConfirmationModal.tsx
+++ app/dashboard/DeleteConfirmationModal.tsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/navigation'
 import { X } from 'lucide-react'
 import { useState } from 'react'
 
-export default function ConfirmationModal (): JSX.Element {
+export default function DeleteConfirmationModal (): JSX.Element {
   const [errorMessage, fireDeleteImage] = useFormState(deleteImageInfoFromDb, undefined)
   const router = useRouter()
   const [canSubmit, setCanSubmit] = useState(false)
@@ -16,21 +16,21 @@ export default function ConfirmationModal (): JSX.Element {
   if (errorMessage !== undefined) {
     console.log(errorMessage)
     router.refresh()
-    const confirmationModal = document.getElementById('confirmation-modal');
+    const confirmationModal = document.getElementById('delete-confirmation-modal');
     (confirmationModal as HTMLDialogElement).close()
   }
 
   return (
-    <dialog id='confirmation-modal'>
+    <dialog id='delete-confirmation-modal'>
       <div className='border px-4 py-6 rounded-lg tracking-wide max-h-[300px] max-w-xl gap-2'>
         <div className='flex items-center justify-between'>
-          <h1 id="display-image-name" className="text-xl font-semibold inline"></h1>
-          <button onClick={() => { (document.getElementById('confirmation-modal') as HTMLDialogElement).close() }}><X className='self-end justify-self-end'/></button>
+          <h1 id="delete-display-image-name" className="text-xl font-semibold inline"></h1>
+          <button onClick={() => { (document.getElementById('delete-confirmation-modal') as HTMLDialogElement).close() }}><X className='self-end justify-self-end'/></button>
         </div>
         <form action={fireDeleteImage} className='gap-2 flex flex-col'>
           <label htmlFor="image-name">Are you sure you want to delete this image? Re-enter the filename to proceed.</label>
           <div className='flex gap-2'>
-            <Input required name="image-name" id="image-name" onInput={(e) => { (e.target as HTMLInputElement).value === document.getElementById('display-image-name')?.innerText ? setCanSubmit(true) : setCanSubmit(false) }}></Input>
+            <Input required name="image-name" id="image-name" onInput={(e) => { (e.target as HTMLInputElement).value === document.getElementById('delete-display-image-name')?.innerText ? setCanSubmit(true) : setCanSubmit(false) }}></Input>
             <Button variant={'ghost'} id="submit-delete" className={`${canSubmit ? 'bg-red-500 hover:bg-red-700' : ''} text-white max-w-28`} type="submit">Delete</Button>
           </div>
           </form>

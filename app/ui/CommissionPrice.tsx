13f23475d67c7cc1ef609f90fb9ae1e9d5027507
Owen Hill <on-hill@outlook.com>
2024-05-09 15:09:39 -0500

    remove placeholder labels and have the prices be positioned uniformly

--- app/ui/CommissionPrice.tsx
+++ app/ui/CommissionPrice.tsx
@@ -5,15 +5,14 @@ import { Input } from './input'
 export default function CommissionPrice ({ idx, pricesCount, setPricesCount, setPriceReqDelete }: { idx: number, pricesCount: number, setPricesCount: Dispatch<SetStateAction<number>>, setPriceReqDelete: Dispatch<SetStateAction<number>> }): JSX.Element {
   return (
     <div id={`price${idx}`} className='flex gap-2'>
-      <label htmlFor={`idx${idx}`}>Placeholder</label>
       <Input id={`idx${idx}`} name={`idx${idx}`} required placeholder='Type of image'></Input>
-      <label htmlFor={`2${idx}`}>Placeholder</label>
       <Input id={`2${idx}`} name={`2${idx}`} required type='number' max='1000000000' placeholder='Amount'></Input>
       <Button id={`plus${idx}`} type='button' onClick={() => { setPricesCount(pricesCount + 1) }}>+</Button>
-      {idx !== 0 &&
-        <Button id={`minus${idx}`} className='bg-red-500 hover:bg-red-600' type='button' onClick={() => {
+      {idx !== 0
+        ? <Button id={`minus${idx}`} className='bg-red-500 hover:bg-red-600' type='button' onClick={() => {
           setPriceReqDelete(idx)
         }}>-</Button>
+        : <div className='w-[100px]'></div>
       }
     </div>
   )
